c72767bf5cf9db85d164c3c12cc65cfb45ba6637
chandra pothireddy <venky.vr.8099@gmail.com>
2024-08-31 07:52:52 +0530

    14. Get Async Data from ngrx store using createSelector and display it in the table - Angular, NGRX

--- src/app/posts/state/posts.state.ts
+++ src/app/posts/state/posts.state.ts
@@ -1,13 +1,12 @@
 import { Posts } from "src/app/models/posts.model"
 
-
 export interface PostState{
     posts: Posts[];
 }
 
-
 export const initialPostsState:PostState = {
     posts:[
-        { id:1, title:'sample title 1', description:'sample description'}
+        { id:1, title:'sample title 1', description:'sample description'},
+        { id:2, title:'sample title 2', description:'sample description 2'}
     ]
 }
