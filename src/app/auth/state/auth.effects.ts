40f728e54213e1eb5a8a34c5d23abca93b315bd5
chandra pothireddy <venky.vr.8099@gmail.com>
2024-09-06 00:57:38 +0530

    30. Navigating the page using routes with Effects after login success in Angular Ngrx Application

--- src/app/auth/state/auth.effects.ts
+++ src/app/auth/state/auth.effects.ts
@@ -2,15 +2,21 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { AuthService } from "src/app/services/auth.service";
 import { loginStart, loginSuccess } from "./auth.actions";
-import { catchError, exhaustMap, map, of } from "rxjs";
+import { catchError, exhaustMap, map, of, tap } from "rxjs";
 import { Store } from "@ngrx/store";
 import { AppState } from "src/app/store/app.state";
 import { setErrorMessage, setLoadingSpinner } from "src/app/store/shared/shared.actions";
+import { Router } from "@angular/router";
 
 
 @Injectable()
 export class AuthEffects {
-  constructor(private actions$: Actions, private authService: AuthService, private store:Store<AppState>) {}
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private store: Store<AppState>,
+    private router: Router
+  ) {}
 
   login$ = createEffect(() => {
     return this.actions$.pipe(
@@ -18,18 +24,33 @@ export class AuthEffects {
       exhaustMap((action) => {
         return this.authService.login(action.email, action.password).pipe(
           map((data) => {
+            this.store.dispatch(setLoadingSpinner({ status: false }));
+            this.store.dispatch(setErrorMessage({ message: '' }));
             const user = this.authService.formatUser(data);
-            this.store.dispatch(setLoadingSpinner({status:false}));
-            this.store.dispatch(setErrorMessage({message:''}));
             return loginSuccess({ user });
           }),
           catchError((errResp) => {
-            this.store.dispatch(setLoadingSpinner({status: false}));
-            const errorMessage = this.authService.getErrorMessage(errResp.error.error.message);
-            return of(setErrorMessage({message: errorMessage}));
+            this.store.dispatch(setLoadingSpinner({ status: false }));
+            const errorMessage = this.authService.getErrorMessage(
+              errResp.error.error.message
+            );
+            return of(setErrorMessage({ message: errorMessage }));
           })
         );
       })
     );
   });
+
+  loginRedirect$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(loginSuccess),// this effect will call whenever dispatch this loginSuccess 
+        tap((action) => {
+          this.router.navigate(['/']);
+        })
+      );
+    },
+    { dispatch: false }
+  );
+  // here the disptach false means, it will not dispatch any action after this effect.
 }
