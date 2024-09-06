0242d3b77858b0598ad771e191c15881d03d5f77
chandra pothireddy <venky.vr.8099@gmail.com>
2024-09-06 10:40:57 +0530

    32. Add Signup Effects to make API call and save the user token in the ngrx state

--- src/app/auth/state/auth.effects.ts
+++ src/app/auth/state/auth.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { AuthService } from "src/app/services/auth.service";
-import { loginStart, loginSuccess } from "./auth.actions";
+import { loginStart, loginSuccess, signupStart, signupSuccess } from "./auth.actions";
 import { catchError, exhaustMap, map, of, tap } from "rxjs";
 import { Store } from "@ngrx/store";
 import { AppState } from "src/app/store/app.state";
@@ -44,13 +44,48 @@ export class AuthEffects {
   loginRedirect$ = createEffect(
     () => {
       return this.actions$.pipe(
-        ofType(loginSuccess),// this effect will call whenever dispatch this loginSuccess 
+        ofType(...[loginSuccess, signupSuccess]), // this effect will call whenever dispatch this loginSuccess and signupsuccess
         tap((action) => {
+          this.store.dispatch(setErrorMessage({ message: '' }));
           this.router.navigate(['/']);
         })
       );
     },
-    { dispatch: false }
+    { dispatch: false } // here the disptach false means, it will not dispatch any action after this effect.
   );
-  // here the disptach false means, it will not dispatch any action after this effect.
+
+  signUp$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(signupStart),
+      exhaustMap((action) => {
+        return this.authService.signUp(action.email, action.password).pipe(
+          map((data) => {
+            this.store.dispatch(setLoadingSpinner({ status: false }));
+            const user = this.authService.formatUser(data);
+            return signupSuccess({ user });
+          }),
+          catchError((errResp) => {
+            this.store.dispatch(setLoadingSpinner({ status: false }));
+            const errorMessage = this.authService.getErrorMessage(
+              errResp.error.error.message
+            );
+            return of(setErrorMessage({ message: errorMessage }));
+          })
+        );
+      })
+    );
+  });
+  // in login, we are doing this action so no need to make another method to redirect, so lets comment here
+  /*signUpRedirect$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(signupSuccess), // this effect will call whenever dispatch this signupSuccess
+        tap((action) => {
+          this.store.dispatch(setErrorMessage({ message: '' }));
+          this.router.navigate(['/']);
+        })
+      );
+    },
+    { dispatch: false } // here the disptach false means, it will not dispatch any action after this effect.
+  );*/
 }
