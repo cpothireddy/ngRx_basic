77415bb925eaa0326f1da224fde5ab8fb7da2919
chandra pothireddy <venky.vr.8099@gmail.com>
2024-09-08 09:14:22 +0530

    49. Ngrx Entity Configuration sort Comparer. Add extra store data in an entity

--- src/app/posts/state/posts.state.ts
+++ src/app/posts/state/posts.state.ts
@@ -1,18 +1,26 @@
 import { createEntityAdapter, EntityState } from "@ngrx/entity";
 import { Posts } from "src/app/models/posts.model";
 
-export interface PostsState extends EntityState<Posts>{};
+export interface PostsState extends EntityState<Posts>{
+    count:number;
+};
+//if you want to add any custom data then createEntityAdapter should expect a primary unique Id
+export const postsAdapter = createEntityAdapter<Posts>({
+    sortComparer: sortByName
+});
 
-export const postsAdapter = createEntityAdapter<Posts>();
+export const initialState: PostsState = postsAdapter.getInitialState({
+    count: 0,
+});
 
-export const initialState: PostsState = postsAdapter.getInitialState();
-
-
-
-// export interface PostState{
-//     posts: Posts[];
-// }
-
-// export const initialPostsState:PostState = {
-//     posts:null
-// }
+export function sortByName(a: Posts, b: Posts): number {
+  //return a.title.localeCompare(b.title); // ascending order
+  const compare = a.title.localeCompare(b.title);
+  if (compare > 0) {
+    return -1;
+  }
+  if (compare < 0) {
+    return 1;
+  }
+  return compare;
+}
